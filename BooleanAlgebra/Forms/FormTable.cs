cbb1baef92d07c8d0d34c73e163706a5c1f0914f
PolShestogo <99017989+PolShestogo@users.noreply.github.com>
2022-10-28 02:34:04 +0300

    Форма для таблицы истинности или что там

---
+++ BooleanAlgebra/Forms/FormTable.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Library;
+
+namespace Forms
+{
+    public partial class FormTable : Form
+    {
+        public FormTable()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonTable_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int count = int.Parse(BoolN.Text);
+                TruthTable table = new TruthTable(count);
+                Rezult.Text = Convert.ToString(table);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Некорректный ввод / Введенное значение за границами диапазона ( от 1 до 26)");
+            }
+        }
+
+        private void buttonHelp_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Программа позволят построить таблицу истинности для введенного ползователем количества логических переменных."
+                + "\nПрограмма имеет удовлетворительную скороть работы только на диапазоне значений логических переменных от 1 до 26."
+                + "\nПри выходе введенного значения из указанного диапазона или некорректном вводе значения количества логических переменных"
+                + "\nбудет показано сообщение об ошибке."
+                , "Справка");
+        }
+    }
+}
