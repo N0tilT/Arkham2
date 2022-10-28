a0e6469df869c62b4b1232900bbdd6e25a09e142
N0tilT <timtompson3241@gmail.com>
2022-10-28 11:47:27 +0300

    individual tests

--- TrainingOOP/TestingClassesCA/Program.cs
+++ TrainingOOP/TestingClassesCA/Program.cs
@@ -15,21 +15,21 @@ namespace TestingClassesCA
             Console.WriteLine("_____\nТестирование класса Student\n_____");
             test.TestStudentProps();
 
-            Console.WriteLine("_____\nТестирование класса Teacher\n_____");
-            test.TeacherTest(); 
-            
-            Console.WriteLine("_____\nТестирование класса Complex\n_____");
-            test.TestComplex();
-            /*
-            Console.WriteLine("_____\nТестирование класса Vehicle\n_____");
-            test.TestVehicleProps();
-            test.TestVehicleUsed();
+            //Console.WriteLine("_____\nТестирование класса Teacher\n_____");
+            //test.TeacherTest(); 
 
-           
+            //Console.WriteLine("_____\nТестирование класса Complex\n_____");
+            //test.TestComplex();
 
-            Console.WriteLine("_____\nТестирование класса Matrix\n_____");
-            test.TestMatrix();
-            */
+            //Console.WriteLine("_____\nТестирование класса Vehicle\n_____");
+            //test.TestVehicleProps();
+            //test.TestVehicleUsed();
+
+
+
+            //Console.WriteLine("_____\nТестирование класса Matrix\n_____");
+            //test.TestMatrix();
+
             Console.ReadKey();
         }
     }
