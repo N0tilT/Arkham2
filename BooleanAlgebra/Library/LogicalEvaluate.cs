c5cc34a6a4a3c068c04221ac47888cca50f6e0f9
N0tilT <timtompson3241@gmail.com>
2022-11-10 21:20:41 +0300

    LogicalFunctions

---
+++ BooleanAlgebra/Library/LogicalEvaluate.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class LogicalEvaluate
+    {
+        Stack<string> operations = new Stack<string>();
+        public Sensor EvaluateTrurhTable(TruthTable table,List<string> tokens)
+        {
+            bool[] result = new bool[table.Table.Length];
+            int i = 0;
+            foreach (Sensor row in table.Table) { result[i] = EvaluateEquation(SetValues(tokens, row)); i++; }
+            return Sensor.Custom(result);
+        }
+        public bool EvaluateEquation(List<string> tokens)
+        {
+            for (int i = 0; i < tokens.Count; i++)
+            {
+                operations.Push(tokens[i]);     //z
+                string curToken = operations.Peek();
+                switch (curToken)
+                {
+                    case "-":
+                        operations.Pop();   // "-"
+                        bool eval = operations.Peek() == "True";  // x
+                        operations.Pop();
+                        operations.Push((!(eval)).ToString());   //+(-x)
+                        break;
+                    case "+":
+                        operations.Pop();   //+
+                        bool eval1 = operations.Peek() == "True";
+                        operations.Pop();
+                        bool eval2 = operations.Peek() == "True";
+                        operations.Pop();
+                        operations.Push((eval1 || eval2).ToString());
+                        break;
+                    case "*":
+                        operations.Pop();   //*
+                        bool evalA = operations.Peek() == "True";
+                        operations.Pop();
+                        bool evalB = operations.Peek() == "True";
+                        operations.Pop();
+                        operations.Push((evalA && evalB).ToString());
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return operations.Pop() =="True";
+        }
+
+
+
+        public List<string> SetValues(List<string> tokens, Sensor values)
+        {
+            List<string> answer = new List<string>();
+            for (int i = 0; i < tokens.Count; i++)
+            {
+                if (tokens[i] != "-" && tokens[i] != "+" && tokens[i] != "*")
+                {
+                    answer.Add(values.List[(int)(tokens[i].ToCharArray()[0]) - 65].ToString());
+                }
+                else answer.Add(tokens[i]);
+            }
+            return answer;
+        }
+    }
+}
---
+++ BooleanAlgebra/Library/LogicalParser.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class LogicalParser
+    {
+        string[] tokens;
+        Stack<string> stack = new Stack<string>();
+        List<string> parsed = new List<string>();
+        bool n;
+
+        public List<string> Parse(string input)
+        {
+            tokens = input.Split(' ');
+
+            foreach(string item in tokens)
+            {
+                if (item == "(") stack.Push(item);
+                else if(item == ")")
+                {
+                    while (stack.Count != 0 && stack.Peek() != "(") parsed.Add(stack.Pop());
+                    stack.Pop();
+                }
+                else if (isOperator(item))
+                {
+                    while (stack.Count != 0 && Priority(stack.Peek()) >= Priority(item)) parsed.Add(stack.Pop());
+                    stack.Push(item);
+                }
+                else parsed.Add(item);
+            }
+            while (stack.Count != 0) parsed.Add(stack.Pop());
+            
+
+            return parsed;
+        }
+
+        static int Priority(string c)
+        {
+            if (c == "-")
+            {
+                return 2;
+            }
+            else if (c == "+" || c == "*")
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        static bool isOperator(string c)
+        {
+            if (c == "+" || c == "-" || c == "*" )
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
