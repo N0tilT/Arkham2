45c631d8d01f69fd76dcb7dff9bb67d676528e3c
vadimyt <pvcs@yandex.ru>
2022-11-11 10:33:52 +0300

    Matrix update

--- TrainingOOP/TrainingOOP/Matrix.cs
+++ TrainingOOP/TrainingOOP/Matrix.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -9,6 +13,7 @@ namespace TrainingOOP
 {
     public class Matrix    //Вадим
     {
+
         #region fields
         private int n;
         private double[,] mas;
@@ -167,6 +172,38 @@ namespace TrainingOOP
             return PowMatrix(a, b);
         }
 
+        //Возведение матрицы в степень?
+        private static Matrix FastPowMatrix(Matrix a, int b)
+        {
+            Matrix answ = a;
+            int s = b;
+            int c = 1;
+            while (s > 1)
+            {
+                if (s % 2 == 1)
+                {
+                    s--;
+                }
+                while (s % 2 == 0)
+                {
+                    answ = answ * answ;
+                    s = s / 2;
+                    c = c * 2;
+                }
+            }
+
+            for (int l = 1; l < b-c+1; l++)
+            {
+                answ = answ * a;
+            }
+            return answ;
+        }
+
+        public static Matrix operator &(Matrix a, int b)
+        {
+            return FastPowMatrix(a, b);
+        }
+
         //Функция расчета алгебаического дополнения элемента матрицы
         private static double AlgComplementA(Matrix A, int k, int m)
         {
@@ -200,6 +237,53 @@ namespace TrainingOOP
                 {
                     det += A[i, 0] * AlgComplementA(A, i, 0);
                 }
+            }
+            return det;
+        }
+        private static Matrix CopyMatrix(Matrix a)
+        {
+            Matrix b = new Matrix(a.N);
+            for (int i = 0; i < a.N; i++)
+                for (int j = 0; j < a.N; j++)
+                {
+                    b[i, j] = a[i, j];
+                }
+                    return b;
+        }
+        //Вычисление определителя методом Гаусса
+        public static double Gauss(Matrix c)
+        {
+            Matrix A = CopyMatrix(c);
+            double[][] B = new double[1][];
+            B[0] = new double[A.n];
+            double det = 1;
+            const double EPS = 1E-9;
+            for (int i = 0; i < A.n; ++i)
+            {
+                int k = i;
+                for (int j = i + 1; j < A.n; ++j)
+                    if (Math.Abs(A[j,i]) > Math.Abs(A[k,i]))
+                        k = j;
+                if (Math.Abs(A[k,i]) < EPS)
+                {
+                    det = 0;
+                    break;
+                }
+                for (int z = 0; z < A.n; z++)
+                {
+                    B[0][z] = A[i,z];
+                    A[i,z] = A[k,z];
+                    A[k,z] = B[0][z];
+                }
+                if (i != k)
+                    det = -det;
+                det *= A[i,i];
+                for (int j = i + 1; j < A.n; ++j)
+                    A[i,j] /= A[i,i];
+                for (int j = 0; j < A.n; ++j)
+                    if ((j != i) && (Math.Abs(A[j,i]) > EPS))
+                        for (k = i + 1; k < A.n; ++k)
+                            A[j,k] -= A[i,k] * A[j,i];
             }
             return det;
         }
