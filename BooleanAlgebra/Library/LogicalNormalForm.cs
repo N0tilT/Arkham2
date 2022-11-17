e157d08f662d00dc7ad79a15b1479527a1e3ec80
vadimyt <pvcs@yandex.ru>
2022-11-17 19:52:24 +0300

    NormalF

---
+++ BooleanAlgebra/Library/LogicalNormalForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    internal class LogicalNormalForm
+    {
+        public string SDNF()
+        {
+        string answer;
+
+        return answer;
+        }
+
+        public string SKNF()
+        {
+        string answer;
+
+        return answer;
+        }
+    }
+}
