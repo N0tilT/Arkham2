7e7ca85e84bb634d44b45af8b6347f144c4738f9
N0tilT <timtompson3241@gmail.com>
2022-11-21 19:24:57 +0300

    Remove+Done Card

--- Timelon/TimelonWPF/ApplicationViewModel.cs
+++ Timelon/TimelonWPF/ApplicationViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
@@ -9,13 +10,35 @@ using TimelonWPF.Core;
 
 namespace TimelonWPF
 {
-    public class AplicationViewModel : INotifyPropertyChanged
+    public class ApplicationViewModel : INotifyPropertyChanged
     {
         private Card _selectedCard;
         private CardList _selectedList;
         private Manager _listManager;
-        ObservableCollection<CardList> lists = new ObservableCollection<CardList>();
-        ObservableCollection<Card> cards = new ObservableCollection<Card>();
+        private ObservableCollection<CardList> _lists = new ObservableCollection<CardList>();
+
+        private ObservableCollection<Card> _defaultCards = new ObservableCollection<Card>();
+        private ObservableCollection<Card> _doneCards = new ObservableCollection<Card>();
+
+        private ObservableCollection<Manager> _manager = new ObservableCollection<Manager>();
+
+        private RelayCommand removeCommand;
+        public RelayCommand RemoveCommand
+        {
+            get
+            {
+                return removeCommand ??
+                    (removeCommand = new RelayCommand(obj =>
+                    {
+                        Card rCard = obj as Card;
+                        if (rCard != null)
+                        {
+                            DefaultCards.Remove(rCard);
+                        }
+                    },
+                    (obj) => DefaultCards.Count > 0));
+            }
+        }
 
         private RelayCommand cardDoneCommand;
 
@@ -26,8 +49,11 @@ namespace TimelonWPF
                 return cardDoneCommand ??
                     (cardDoneCommand = new RelayCommand(obj =>
                     {
-                        _selectedCard.IsCompleted = true;
-                        SelectedCard = _selectedCard;
+                        Card completed = obj as Card;
+                        if (!completed.IsCompleted)
+                        {       
+                            SelectedCard.IsCompleted = true;
+                        }
                     }));
             }
         }
@@ -43,8 +69,8 @@ namespace TimelonWPF
                     {
                         TextBox tmp = obj as TextBox;
                         CardList newList = CardList.Make(tmp.Text);
-                        _listManager.SetList(newList);
-                        lists.Add(newList);
+                        ListManager.SetList(newList);
+                        Lists.Add(newList);
                         SelectedList = newList;
                     }));
             }
@@ -62,7 +88,7 @@ namespace TimelonWPF
                         TextBox tmp = obj as TextBox;
                         Card newCard = Card.Make(tmp.Text);
                         SelectedList.Set(newCard);
-                        cards.Add(newCard);
+                        DefaultCards.Add(newCard);
                         SelectedCard = newCard;
                     }));
             }
@@ -78,14 +104,16 @@ namespace TimelonWPF
                     (searchCardCommand = new RelayCommand(obj =>
                     {
                         TextBox tmp = obj as TextBox;
-                        SelectedList = new CardList(0, "searchRuselt", true, _listManager.SearchByContent(tmp.Text));
+                        SelectedList = new CardList(0, "searchResult", true, _listManager.SearchByContent(tmp.Text));
                     }));
             }
         }
 
 
-        public ObservableCollection<CardList> Lists { get { return lists; } set { lists = value; } }
-        public ObservableCollection<Card> Cards { get { return cards; } set { cards = value; } }
+        public ObservableCollection<CardList> Lists { get { return _lists; } set { _lists = value; } }
+        public ObservableCollection<Card> DefaultCards { get { return _defaultCards; } set { _defaultCards = value; } }
+        public ObservableCollection<Card> DoneCards { get { return _doneCards; } set { _doneCards = value; } }
+        public Manager ListManager { get { return _listManager; } set { _listManager = value; } }
 
         public Card SelectedCard
         {
@@ -103,21 +131,27 @@ namespace TimelonWPF
             set
             {
                 _selectedList = value;
-                cards.Clear();
-                foreach (KeyValuePair<int, Card> item in SelectedList.All)
-                    cards.Add(item.Value);
+                _defaultCards.Clear();
+                _doneCards.Clear();
+
+                foreach (Card item in SelectedList.GetListDefault())
+                    _defaultCards.Add(item);
+
+                foreach (Card item in SelectedList.GetListCompleted())
+                    _doneCards.Add(item);
+
                 OnPropertyChanged("SelectedList");
             }
         }
 
-        public AplicationViewModel()
+        public ApplicationViewModel()
         {
-            _listManager = Manager.Instance;
+            ListManager = Manager.Instance;
 
-            _selectedList = _listManager.All[0];
+            SelectedList = ListManager.All[0];
 
             foreach (KeyValuePair<int, CardList> item in _listManager.All)
-                lists.Add(item.Value);
+                _lists.Add(item.Value);
 
             _selectedList.Set(Card.Make("Test1"));
             _selectedList.Set(Card.Make("Test2"));
@@ -125,7 +159,7 @@ namespace TimelonWPF
 
 
             foreach (KeyValuePair<int, Card> item in SelectedList.All)
-                cards.Add(item.Value);
+                DefaultCards.Add(item.Value);
 
         }
 
